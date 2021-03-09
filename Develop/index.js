d75943ea74cc42ba816fd2b7f11968fdcd688935
dboren <danielboren@att.net>
2021-03-09 17:00:08 -0600

    separated inquirer prompts into their own variable and restructured init and write to file functions to use the path and writeFileSync methods to properly format data using the generateMarkdown module

--- Develop/index.js
+++ Develop/index.js
@@ -2,72 +2,76 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-
+const path = require('path');
 
 // TODO: Create an array of questions for user input
 const questions = ["What will be the title of this document?", "Please enter a description of the project.", "What will be in the table of contents?", "How is this app installed?", "How is this app used?", "Which license does this app use?", "Who contributed to this project?", "How can we contact you with additional questions?"];
 
+const prompts = [{
+    type: 'input',
+    name: 'title',
+    message: questions[0]
+},
+{
+    type: 'input',
+    name: 'description',
+    message: questions[1]
+},
+{
+    type: 'input',
+    name: 'contents',
+    message: questions[2]
+},
+{
+    type: 'input',
+    name: 'install',
+    message: questions[3]
+},
+{
+    type: 'input',
+    name: 'use',
+    message: questions[4]
+},
+{
+    type: 'input',
+    name: 'license',
+    message: questions[5],
+},
+{
+    type: 'input',
+    name: 'contributors',
+    message: questions[6]
+},
+{
+    type: 'input',
+    name: 'contact',
+    message: questions[7]
+}
+
+];
+
 // console.log("First question: " + questions[0]);
 
 // TODO: Create a function to write README file
-function writeToFile(data) {
-    const fileName = `README.md`
-    fs.writeFile(fileName, JSON.stringify(data, null, '\t'), function(err, result) {
-        if(err) console.log('error', err);
-      }) 
-        generateMarkdown(data);
+function writeToFile(fileName, data) {
+    console.log("writeToFile");
+    console.log(data);
+    // const fileName = `README.md`
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+      
         // return fileName;
     
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: questions[0]
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: questions[1]
-            },
-            {
-                type: 'input',
-                name: 'contents',
-                message: questions[2]
-            },
-            {
-                type: 'input',
-                name: 'install',
-                message: questions[3]
-            },
-            {
-                type: 'input',
-                name: 'use',
-                message: questions[4]
-            },
-            {
-                type: 'input',
-                name: 'license',
-                message: questions[5],
-            },
-            {
-                type: 'input',
-                name: 'contributors',
-                message: questions[6]
-            },
-            {
-                type: 'input',
-                name: 'contact',
-                message: questions[7]
-            }
-        ])
-        .then((data) => {
-            writeToFile(data);
-        })
+    inquirer.prompt(prompts)
+        .then((data) => {writeToFile("README.md", generateMarkdown({...data}))
+        
+        }) 
+        // .then((data) => {
+        //     writeToFile(data);
+        // })
         // console.log("data: " + data);
         // .then(writeToFile());
         // console.log("data: " + data);
--- README.md
+++ README.md
@@ -1,10 +1,27 @@
-{
-	"title": "t",
-	"description": "t",
-	"contents": "t",
-	"install": "t",
-	"use": "t",
-	"license": "t",
-	"contributors": "t",
-	"contact": "t"
-}
+# t
+
+  ## Description
+
+  t
+
+  ## Table of Contents
+
+  t
+
+  ## How to Install
+
+  t
+
+  ## How to Use this App
+
+  t
+
+  ## Licenses used
+
+  t
+
+  ## Contributors
+
+  -t
+
+  For further information, please reach out to t
